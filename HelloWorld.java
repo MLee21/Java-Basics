a8a2b38d78f8eb7b1c6b25d8ef145d2bdd9e6962
MLee21 <minnie.lee89@gmail.com>
2015-07-26 14:23:35 -0600

    multiple variable assignment. created mad lib story.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,13 @@ import java.io.Console;
 public class HelloWorld {
   public static void main(String[] args) {
     Console console = System.console();
-    String firstName = console.readLine("Enter your name: ");
+    String name = console.readLine("Enter a name: ");
     String adjective = console.readLine("Enter an adjective: ");
-    console.printf("%s is very %s", firstName, adjective);
+    String noun = console.readLine("Enter a noun: ");
+    String adverb = console.readLine("Enter an adjective: ");
+    String verb = console.readLine("Enter a verb ending in -ing: ");
+    console.printf("Your fun short story:\n-------------------\n");
+    console.printf("%s is a %s %s.\n", name, adjective, noun);
+    console.printf("%s is always %s %s.\n", name, adverb, verb);
   }
 }
