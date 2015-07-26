0cc3713f85ee68b6dd138adf9d9777c8ba6633da
MLee21 <minnie.lee89@gmail.com>
2015-07-26 14:42:43 -0600

    using methods.

--- HelloWorld.java
+++ HelloWorld.java
@@ -12,6 +12,10 @@ public class HelloWorld {
     String name = console.readLine("Enter a name: ");
     String adjective = console.readLine("Enter an adjective: ");
     String noun = console.readLine("Enter a noun: ");
+    if (noun.equals("dork")) {
+      console.printf("That language is not allowed. Exiting \n\n");
+      System.exit(0);
+    }
     String adverb = console.readLine("Enter an adjective: ");
     String verb = console.readLine("Enter a verb ending in -ing: ");
     console.printf("Your fun short story:\n-------------------\n");
