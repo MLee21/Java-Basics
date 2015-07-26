73e08394fe3ea7c4ab998d8925b2aaf3e72fdfb1
MLee21 <minnie.lee89@gmail.com>
2015-07-26 13:43:31 -0600

    basic string interpolation. woo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("hello world");
+    String firstName = "Moss";
+    System.out.println("Hello, my name is " + firstName);
   }
 }
