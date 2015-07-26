41c880db6f4133baa47b53e97ba19c9bce2629af
MLee21 <minnie.lee89@gmail.com>
2015-07-26 14:33:54 -0600

    assign integers and parsing integers.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,12 @@ import java.io.Console;
 public class HelloWorld {
   public static void main(String[] args) {
     Console console = System.console();
+    String ageAsString = console.readLine("How old are you? ");
+    int age = Integer.parseInt(ageAsString);
+    if (age < 13) {
+      console.printf("Sorry you must be at least 13 to use this program\n");
+      System.exit(0);
+    }
     String name = console.readLine("Enter a name: ");
     String adjective = console.readLine("Enter an adjective: ");
     String noun = console.readLine("Enter a noun: ");
