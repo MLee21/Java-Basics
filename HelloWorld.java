93400bd1fea463814ce584ed6e4c82b40eaf1798
MLee21 <minnie.lee89@gmail.com>
2015-07-26 14:04:29 -0600

    added file input and output and stored and outputted two variables.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@ import java.io.Console;
 public class HelloWorld {
   public static void main(String[] args) {
     Console console = System.console();
-    String firstName = "Moss";
-    console.printf("Hello, my name is %s\n", firstName);
+    String firstName = console.readLine("Enter your name: ");
+    String adjective = console.readLine("Enter an adjective: ");
+    console.printf("%s is very %s", firstName, adjective);
   }
 }
